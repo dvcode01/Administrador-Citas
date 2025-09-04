250cb7294ca267feb037bc6904dea57da109bb98
david <daviolivero2103@gmail.com>
2025-09-04 15:27:49 -0400

    Chore: add buttons to appointments

--- js/app.js
+++ js/app.js
@@ -87,13 +87,28 @@ class AdminCitas{
             citaSintomas.classList.add('font-normal', 'mb-3', 'text-gray-700', 'normal-case')
             citaSintomas.innerHTML = `<span class="font-bold uppercase">Síntomas: </span> ${sintomas}`;
 
+            // Botones
+            const btnEditar = document.createElement('button');
+            btnEditar.classList.add('py-2', 'px-10', 'bg-indigo-600', 'hover:bg-indigo-700', 'text-white', 'font-bold', 'uppercase', 'rounded-lg', 'flex', 'items-center', 'gap-2');
+            btnEditar.innerHTML = 'Editar <svg fill="none" class="h-5 w-5" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>';
+
+            const btnEliminar = document.createElement('button');
+            btnEliminar.classList.add('py-2', 'px-10', 'bg-red-600', 'hover:bg-red-700', 'text-white', 'font-bold', 'uppercase', 'rounded-lg', 'flex', 'items-center', 'gap-2');
+            btnEliminar.innerHTML = 'Eliminar <svg fill="none" class="h-5 w-5" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>';
+
+            const contenedorBotones = document.createElement('div');
+            contenedorBotones.classList.add('mt-10', 'flex', 'justify-between');
+            contenedorBotones.appendChild(btnEditar);
+            contenedorBotones.appendChild(btnEliminar);
+
             // Agregando la informacion a la cita
             citaDiv.appendChild(citaPaciente);
             citaDiv.appendChild(citaPropietario);
             citaDiv.appendChild(citaEmail);
             citaDiv.appendChild(citaFecha);
             citaDiv.appendChild(citaSintomas);
-
+            citaDiv.appendChild(contenedorBotones);
+            
             // Agregando al contenedor de las citas
             citasContainer.appendChild(citaDiv);
         });
