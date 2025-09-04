3f7da888f8909e03509b7219f2aaeec2e029814e
david <daviolivero2103@gmail.com>
2025-09-04 16:11:58 -0400

    Chore: add appointment values in the inputs

--- js/app.js
+++ js/app.js
@@ -7,8 +7,11 @@ const inputSintomas = document.querySelector('#sintomas');
 const formularioCita = document.querySelector('#formulario-cita');
 const citasContainer = document.querySelector('#citas');
 
+let editando = false;
+
 // Objeto cita
 const cita = {
+    id: crypto.randomUUID(),
     paciente: '',
     propietario: '',
     email: '',
@@ -54,6 +57,7 @@ class AdminCitas{
 
     agregar(cita){
         this.citas = [...this.citas, cita];
+        console.log(citas);
     }
 
     mostrarCitas(){
@@ -91,6 +95,10 @@ class AdminCitas{
             const btnEditar = document.createElement('button');
             btnEditar.classList.add('py-2', 'px-10', 'bg-indigo-600', 'hover:bg-indigo-700', 'text-white', 'font-bold', 'uppercase', 'rounded-lg', 'flex', 'items-center', 'gap-2');
             btnEditar.innerHTML = 'Editar <svg fill="none" class="h-5 w-5" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>';
+            btnEditar.onclick = () => {
+                const clone = structuredClone(cita);
+                cargarEdicion(clone);
+            };
 
             const btnEliminar = document.createElement('button');
             btnEliminar.classList.add('py-2', 'px-10', 'bg-red-600', 'hover:bg-red-700', 'text-white', 'font-bold', 'uppercase', 'rounded-lg', 'flex', 'items-center', 'gap-2');
@@ -148,10 +156,15 @@ function submitCita(e){
         return;
     }
 
+    // Comprueba si se debe editar un registro
+    if(editando){
+        
+        return;
+    }
+
     // Guardando cita
     citas.agregar({...cita});
     new Notificacion({texto: 'Paciente guardado', tipo: 'success'});
-
 
     // Mostrando citas
     citas.mostrarCitas();
@@ -161,56 +174,26 @@ function submitCita(e){
     resetearCitaObj();
 }
 
-function mostrarCitas(){
-    // Limpiar HTML
-    limpiarCitas();
-
-    // Generando citas
-    citas.forEach(cita => {
-        const {id, paciente, propietario, email, fecha, sintomas} = cita;
-
-        const citaDiv = document.createElement('div');
-        citaDiv.classList.add('mx-5', 'my-10', 'bg-white', 'shadow-md', 'px-5', 'py-10', 'rounded-xl');
-
-        const citaPaciente = document.createElement('p');
-        citaPaciente.classList.add('font-normal', 'mb-3', 'text-gray-700', 'normal-case');
-        citaPaciente.innerHTML = `<span class="font-bold uppercase">Paciente: </span> ${paciente}`;
-
-        const citaPropietario = document.createElement('p');
-        citaPropietario.classList.add('font-normal', 'mb-3', 'text-gray-700', 'normal-case');
-        citaPropietario.innerHTML = `<span class="font-bold uppercase">Propietario: </span> ${propietario}`;
-
-        const citaEmail = document.createElement('p');
-        citaEmail.classList.add('font-normal', 'mb-3', 'text-gray-700', 'normal-case')
-        citaEmail.innerHTML = `<span class="font-bold uppercase">E-mail: </span> ${email}`;
-
-        const citaFecha = document.createElement('p');
-        citaFecha.classList.add('font-normal', 'mb-3', 'text-gray-700', 'normal-case')
-        citaFecha.innerHTML = `<span class="font-bold uppercase">Fecha: </span> ${fecha}`;
-
-        const citaSintomas = document.createElement('p');
-        citaSintomas.classList.add('font-normal', 'mb-3', 'text-gray-700', 'normal-case')
-        citaSintomas.innerHTML = `<span class="font-bold uppercase">Síntomas: </span> ${sintomas}`;
-
-        // Agregando la informacion a la cita
-        citaDiv.appendChild(citaPaciente);
-        citaDiv.appendChild(citaPropietario);
-        citaDiv.appendChild(citaEmail);
-        citaDiv.appendChild(citaFecha);
-        citaDiv.appendChild(citaSintomas);
-
-        // Agregando al contenedor de las citas
-        citasContainer.appendChild(citaDiv);
-    });
-}
-
 function resetearCitaObj(){
     // Reiniciar objeto
+    cita.id = crypto.randomUUID()
     cita.paciente =  '';
     cita.propietario =  '';
     cita.email =  '';
     cita.fecha =  '';
     cita.sintomas = '';
+}
+
+function cargarEdicion(citaObj){
+    Object.assign(cita, citaObj);
+
+    inputPaciente.value = citaObj.paciente;
+    inputPropietario.value = citaObj.propietario;
+    inputEmail.value = citaObj.email;
+    inputFecha.value = citaObj.fecha;
+    inputSintomas.value = citaObj.sintomas;
+
+    editando = true;
 }
 
 
