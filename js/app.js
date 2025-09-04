c8e9a1d969a3fa67d54ac99dafd05356141181c2
david <daviolivero2103@gmail.com>
2025-09-03 21:51:23 -0400

    Feat: add input values to the date object

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ const inputPropietario = document.querySelector('#propietario');
 const inputEmail = document.querySelector('#email');
 const inputFecha = document.querySelector('#fecha');
 const inputSintomas = document.querySelector('#sintomas');
+const formularioCita = document.querySelector('#formulario-cita')
 
 // Objeto cita
 const cita = {
@@ -12,8 +13,30 @@ const cita = {
     propietario: '',
     email: '',
     fecha: '',
-    sintomas: []
+    sintomas: ''
 };
+
+// Eventos
+listeners();
+
+function listeners(){
+    inputPaciente.addEventListener('change', datosCita);
+    inputPropietario.addEventListener('change', datosCita);
+    inputEmail.addEventListener('change', datosCita);
+    inputFecha.addEventListener('change', datosCita);
+    inputSintomas.addEventListener('change', datosCita);
+    formularioCita.addEventListener('submit', submitCita);
+}
+
+function datosCita(e){
+    cita[e.target.name] = e.target.value;
+}
+
+function submitCita(e){
+    e.preventDefault();
+
+    
+}
 
 
 
