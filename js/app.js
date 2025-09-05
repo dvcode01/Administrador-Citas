0d076a1e9444f3dc2b28633745e858588365ed34
david <daviolivero2103@gmail.com>
2025-09-04 21:42:21 -0400

    Feat: add function to delete appointments

--- js/app.js
+++ js/app.js
@@ -64,9 +64,20 @@ class AdminCitas{
         this.citas = this.citas.map(cita => cita.id === citaActualizada.id ? citaActualizada : cita);
     }
 
+    eliminar(id){
+        this.citas = this.citas.filter(cita => cita.id !== id);
+        this.mostrarCitas();
+    }
+
     mostrarCitas(){
         // Limpiar HTML
         this.limpiarCitas();
+
+        // Comprobando si no hay citas
+        if(this.citas.length === 0){
+            citasContainer.innerHTML = '<p class="text-xl mt-5 mb-10 text-center">No Hay Pacientes</p>';
+            return;
+        }
 
         // Generando citas
         this.citas.forEach(cita => {
@@ -107,6 +118,9 @@ class AdminCitas{
             const btnEliminar = document.createElement('button');
             btnEliminar.classList.add('py-2', 'px-10', 'bg-red-600', 'hover:bg-red-700', 'text-white', 'font-bold', 'uppercase', 'rounded-lg', 'flex', 'items-center', 'gap-2');
             btnEliminar.innerHTML = 'Eliminar <svg fill="none" class="h-5 w-5" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>';
+            btnEliminar.onclick = () => {
+                this.eliminar(cita.id);
+            }
 
             const contenedorBotones = document.createElement('div');
             contenedorBotones.classList.add('mt-10', 'flex', 'justify-between');
